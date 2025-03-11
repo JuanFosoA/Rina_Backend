6b74a41836eafe7c5b9395a9806e2bcc8271b3be
YioJoe <yioyiomain@gmail.com>
2025-03-11 13:48:45 -0500

    First Commit

---
+++ src/main/java/com/backend/Rina/RinaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Rina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/Rina/RinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Rina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
